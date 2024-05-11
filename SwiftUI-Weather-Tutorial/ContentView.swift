93dbc05fcfcc39680f1b6dabcdca1be6079aed8b
João Pedro Leopoldino <leopoldinodev@gmail.com>
2024-05-11 10:42:49 -0300

    Initial Commit

---
+++ SwiftUI-Weather-Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather-Tutorial
+//
+//  Created by João Pedro Leopoldino on 2024-05-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Weather-Tutorial/SwiftUI_Weather_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_TutorialApp.swift
+//  SwiftUI-Weather-Tutorial
+//
+//  Created by João Pedro Leopoldino on 2024-05-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
